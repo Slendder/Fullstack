f9fc09fc6150b82f144a8633b2461f36a5622fca
Slendder <141583251+Slendder@users.noreply.github.com>
2024-02-20 15:15:11 -0300

    as

---
+++ src/Part2/App.jsx
@@ -0,0 +1,15 @@
+const App = ({ notes }) => {
+    console.log(notes);
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
