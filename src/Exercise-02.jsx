c4001e32689f1e5f56d1128145f8bf67d09e8379
Slendder <141583251+Slendder@users.noreply.github.com>
2024-02-20 12:47:18 -0300

    wtf

---
+++ src/Exercise-02.jsx
@@ -0,0 +1,58 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const Header = (prop) => {
+    return (
+      <div>
+        <p>{prop.course}.</p>
+      </div>
+    );
+  };
+
+  const Part = (prop) => {
+    return (
+      <>
+        <p>
+          {" "}
+          {prop.name}: {prop.content} exercises.
+        </p>
+      </>
+    );
+  };
+
+  const Content = () => {
+    return (
+      <div>
+        <Part name={part1} content={exercises1} />
+        <Part name={part2} content={exercises2} />
+        <Part name={part3} content={exercises3} />
+      </div>
+    );
+  };
+
+  const Total = (prop) => {
+    return (
+      <div>
+        <p>Total number of exercise: {prop.total_exercises}.</p>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[part1, exercises1, part2, exercises2, part3, exercises3]}
+      />
+      <Total total_exercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Exercise-05.jsx
@@ -0,0 +1,46 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const Content = (prop) => {
+    return (
+      <>
+        <p>
+          {prop.part[0].name}: {prop.part[0].exercises}.
+        </p>
+        <p>
+          {prop.part[1].name}: {prop.part[1].exercises}.
+        </p>
+        <p>
+          {prop.part[2].name}: {prop.part[2].exercises}.
+        </p>
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <Content part={course.parts} />
+
+      <p>Total Parts: {course.parts.length}.</p>
+    </div>
+  );
+};
+
+export default App;
