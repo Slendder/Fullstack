afa31dcb536320f3e78c705450cb3b7b0d9bc680
Slender <bautista.prieto.hoses@gmail.com>
2024-03-05 11:12:18 -0300

    s

--- src/Part2/App.jsx
+++ src/Part2/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = ({props}) => {
-  const [notes, setNotes] = useState(notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
-  const [newNote, setNewNote] = useState("a new note...");
 
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -24,7 +34,7 @@ const App = ({props}) => {
   };
   const notesToShow = showAll
     ? notes
-    : notes.filter(note => note.important === true)
+    : notes.filter((note) => note.important === true);
   return (
     <div>
       <h1>Notes</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import axios from "axios";
+import App from "./Part2/App";
 
-import App from "./Part2/Exercise_06-10";
-
-const promise = axios.get("http://localhost:3001/notes");
-console.log(promise);
-
-const promise2 = axios.get("http://localhost:3001/foobar");
-console.log(promise2);
-
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: Math.random() < 0.5,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: Math.random() < 0.5,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: Math.random() < 0.5,
-  },
-];
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
