3b92342eaaa2358b8689d08214d55c00789c15f7
Slendder <bautista.prieto.hoses@gmail.com>
2024-03-10 19:49:07 -0300

    Exercise11

--- src/Part2/Exercise_11.jsx
+++ src/Part2/Exercise_11.jsx
@@ -21,12 +21,12 @@ const App = () => {
 
   useEffect(() => {
     console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
+    axios.get("http://localhost:3001/persons").then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
   }, []);
-  
+
   console.log("render", notes.length, "notes");
   const addNote = (event) => {
     event.preventDefault();
